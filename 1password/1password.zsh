834590f985fc2f30fc43c589bc0c1aa04f41f025
Matthew Turney <matthew.a.turney@gmail.com>
2023-11-29 19:40:20 -0600

    Support linux in 1password setup

--- 1password/1password.zsh
+++ 1password/1password.zsh
@@ -1,18 +1,19 @@
-#!/bin/zsh
+#!/bin/env zsh
 
-ONEPASSWORD_DIR=~/.1password
-AGENT_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+local ONEPASSWORD_DIR=~/.1password
+local AGENT_SOCK=$ONEPASSWORD_DIR/agent.sock
+local MACOS_SOCK="~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+local errorMessage="1Password agent not running"
 
 if [[ ! -d $ONEPASSWORD_DIR ]]; then
   mkdir -p $ONEPASSWORD_DIR
 fi
 
-if [[ -S $AGENT_SOCK ]]; then
-  if [[ ! -L $ONEPASSWORD_DIR/agent.sock ]]; then
-    ln -s $AGENT_SOCK $ONEPASSWORD_DIR/agent.sock
-  fi
-
-  export SSH_AUTH_SOCK=$ONEPASSWORD_DIR/agent.sock
-else
-  echo "1Password agent socket not found. Skipping setup."
+# If on MacOS, symlink the agent socket to a common location
+if [[ ! -S $AGENT_SOCK && $_macos = "true" && -S $MACOS_SOCK ]]; then
+  ln -s $MACOS_SOCK $AGENT_SOCK
+elif [[ ! -S $AGENT_SOCK  && !$_macos = "true" ]]; then
+  echo $errorMessage
 fi
+
+export SSH_AUTH_SOCK=$AGENT_SOCK
